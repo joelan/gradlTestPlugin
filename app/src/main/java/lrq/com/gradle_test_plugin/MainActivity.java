3e416276e06abcdd8ea37436e8032aaf38a9eef3
joelan <joe544351900@sina.cn>
2016-10-08 14:34:36 +0800

    插件初始化

---
+++ app/src/main/java/lrq/com/gradle_test_plugin/MainActivity.java
@@ -0,0 +1,13 @@
+package lrq.com.gradle_test_plugin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ mygradleplugin/src/main/groovy/com/lrq/joe/testgradle/MytestPlugin.groovy
@@ -0,0 +1,14 @@
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+
+public  class MytestPlugin  implements Plugin<Project> {
+
+
+    @Override
+    void apply(Project project) {
+        project.task('testTask') << {
+            println "Hello this Joe's gradle plugin"
+        }
+    }
+}
